defb237c667a1f4fb644179631f14870598369b0
akhil singla <akhil.singla@logmein.com>
2021-12-31 14:02:25 +0530

    udpate

--- EmployeeManagementRestAPI/src/main/java/com/greatlearning/mainApp/EmployeeManagementRestApiApplication.java
+++ EmployeeManagementRestAPI/src/main/java/com/greatlearning/mainApp/EmployeeManagementRestApiApplication.java
@@ -1,18 +1,16 @@
 package com.greatlearning.mainApp;
 
-import com.greatlearning.entity.Employee;
 import com.greatlearning.entity.Role;
 import com.greatlearning.entity.User;
-import com.greatlearning.service.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.greatlearning.service.PasswordEncoderService;
+import com.greatlearning.service.UserService;
+import com.greatlearning.service.UserServiceJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
